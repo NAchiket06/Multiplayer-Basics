0a22dd794cc91b8d147f6d42aee4470def658fa7
Nachiket Jadhav <nachiketjadhav123@yahoo.co.in>
2021-09-03 20:46:05 +0530

    Completed Basics

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -28,45 +28,64 @@ public class MyNetworkPlayer : NetworkBehaviour
         displayName = newDisplayName;
     }
 
+
+
     [Server]
     public void SetDisplayColour(Color newDisplayColour)
     {
         displayColour = newDisplayColour;
     }
 
+
+
     [Command]
     private void CmdSetDisplayName(string newDisplayName)
     {
         //server Validation here
+        if(newDisplayName.Length<4 || newDisplayName.Length > 12)
+        {
+            return;
+        }
 
-        RpcLogNewName(newDisplayName); // all clients log using new name
+        RpcLogNewName(newDisplayName); 
+
         SetDisplayName(newDisplayName);
     }
 
     #endregion
 
     #region Client
+
+
     private void HandleDisplayNameUpdated(string oldName, string newName)
     {
         displayNameText.text = newName;
     }
+
+
 
     private void HandleDisplayColourUpdated(Color oldColour, Color newColour)
     {
         displayColourRenderer.material.SetColor("_Color", newColour);
     }
     
+
+
     [ContextMenu("Set Name")]
     private void SetMyName()
     {
         CmdSetDisplayName("New Name");
     }
 
+
+
     [ClientRpc]
     private void RpcLogNewName(string newDisplayName)
     {
         Debug.Log(newDisplayName);
     }
+
+
 
     #endregion
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : NetworkBehaviour
+{
+
+    [SerializeField] private NavMeshAgent agent = null;
+
+    [SerializeField] private Camera mainCamera;
+
+    #region Server
+    [Command]
+    private void CmdMove(Vector3 position)
+    {
+        NavMeshHit hit;
+        if (!NavMesh.SamplePosition(position, out hit, 10, NavMesh.AllAreas))
+        {
+            return;
+        }
+
+
+        agent.SetDestination(hit.position);
+    }
+    #endregion
+
+    #region Client
+
+    public override void OnStartAuthority()
+    {
+        base.OnStartAuthority();
+        mainCamera = Camera.main;
+    }
+
+    [ClientCallback]
+    private void Update()
+    {
+        if(!hasAuthority)
+        {
+            return;
+        }
+        if (!Input.GetMouseButtonDown(0))
+        {
+            return;
+        }
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+        {
+            return;
+        }
+
+        CmdMove(hit.point);
+
+    }
+
+    #endregion
+}

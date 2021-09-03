41990c972aba6904918f00f87db4e0a8d8b8e337
Nachiket Jadhav <nachiketjadhav123@yahoo.co.in>
2021-09-03 14:58:10 +0530

    Remote Actions implemented

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,21 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>();
+        player.SetDisplayName($"Player {numPlayers}");
+        player.SetDisplayColour(new Color(
+            Random.value, 
+            Random.value, 
+            Random.value));
+
+    }
+}
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using TMPro;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColourRenderer = null;
+
+    //hooks call the function whenever the value of var is updated.
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))]
+    [SerializeField]
+    private string displayName = "Missing Name";
+
+    [SyncVar(hook = nameof(HandleDisplayColourUpdated))]
+    [SerializeField]
+    private Color displayColour = Color.black;
+
+
+    #region Server
+
+    //only executed in servers and not in clients
+    [Server]
+    public void SetDisplayName(string newDisplayName)
+    {
+        displayName = newDisplayName;
+    }
+
+    [Server]
+    public void SetDisplayColour(Color newDisplayColour)
+    {
+        displayColour = newDisplayColour;
+    }
+
+    [Command]
+    private void CmdSetDisplayName(string newDisplayName)
+    {
+        //server Validation here
+
+        RpcLogNewName(newDisplayName); // all clients log using new name
+        SetDisplayName(newDisplayName);
+    }
+
+    #endregion
+
+    #region Client
+    private void HandleDisplayNameUpdated(string oldName, string newName)
+    {
+        displayNameText.text = newName;
+    }
+
+    private void HandleDisplayColourUpdated(Color oldColour, Color newColour)
+    {
+        displayColourRenderer.material.SetColor("_Color", newColour);
+    }
+    
+    [ContextMenu("Set Name")]
+    private void SetMyName()
+    {
+        CmdSetDisplayName("New Name");
+    }
+
+    [ClientRpc]
+    private void RpcLogNewName(string newDisplayName)
+    {
+        Debug.Log(newDisplayName);
+    }
+
+    #endregion
+}
+
